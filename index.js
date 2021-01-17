5ef293906f45f195ae5c794b9f374a837525e7dd
AMVNet <29984642+AMVNet@users.noreply.github.com>
2021-01-17 16:47:15 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// Generate a random number
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+// Display random dice image
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+// Change the title to display a winner or draw
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML ="🚩 Player 1 Wins!!";
+} else if ( randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML ="Player 2 Wins!! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML ="It's a Draw!!";
+}
